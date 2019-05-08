c5ffd5b52c37d8a2d2fe7617419882bc95bd004d
Chuong Phan <chuong@whl.travel>
2019-05-08 17:14:21 +0700

    redux action reducer connect middleware async

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,58 +1,85 @@
 import React , { Component } from "react";
+import {connect} from 'react-redux';
 import CardList from "../Components/CardList";
 import SearchBox from "../Components/SearchBox";
 import Scroll from "../Components/Scroll";
 import ErrorBoundtry from "../Components/ErrorBoundry";
 
+import {setSearchField , requestRobots} from '../actions';
 
+
+const mapStateToProps = state =>{
+	return {
+		searchField: state.searchRobots.searchField,
+		robots: state.requestRobots.robots,
+		isPending:state.requestRobots.isPending,
+		error:state.requestRobots.error
+	}
+}
+
+const mapDispatchToProps = (dispatch) => {
+	return{
+		onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+		onRequestRobots:() => dispatch(requestRobots())
+	}
+	
+}
 
 class App extends Component {
-	constructor(){
+	/*constructor(){
 		super()
 		this.state = {
 			robots : [],
-			searchfield : "",
+			//searchfield : "",
 		}
-	}
+	}*/
 
 	componentDidMount(){
-		fetch('http://jsonplaceholder.typicode.com/users')
+		/*fetch('http://jsonplaceholder.typicode.com/users')
 			.then(response=>{
 				return response.json();
 			})
 			.then(users=>{
 				this.setState({robots:users});
-			})
+			})*/
+
+		this.props.onRequestRobots();
 	}
 
-	onSearchChange = (event) =>{
+	/*onSearchChange = (event) =>{
 		this.setState({searchfield: event.target.value})
 	}
-
+*/
 	render(){
-		const {robots, searchfield} = this.state;
+		//const {robots, searchfield} = this.state;
+
+		//const {robots} = this.state;
+		const {searchField , onSearchChange , robots , isPending} = this.props;
+
 		const filteredRobots = robots.filter(robot=>{
-			return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+			return robot.name.toLowerCase().includes(searchField.toLowerCase())
 		})
 
-		if(robots.length ===0){
-			return <h1>Loading</h1>
-		}else{
-			return (
-			<div className="tc">
-			<h1>Robo Friend</h1>
-			<SearchBox searchChange={this.onSearchChange}/>
-			<Scroll>
-				<ErrorBoundtry>
-					<CardList robots={filteredRobots}/>
-				</ErrorBoundtry>
-			</Scroll>
-			</div>
-			);
-		}
+
+		return isPending ? 
+		<h1>Loading</h1> :
+
+		 (
+		<div className="tc">
+		<h1>Robo Friend</h1>
+		{/*<SearchBox searchChange={this.onSearchChange}/>*/}
+		<SearchBox searchChange={onSearchChange}/>
+		<Scroll>
+			<ErrorBoundtry>
+				<CardList robots={filteredRobots}/>
+			</ErrorBoundtry>
+		</Scroll>
+		</div>
+		);
+		
 	}
 	
 
 }
 
-export default App;
+export default connect(mapStateToProps,mapDispatchToProps)(App);
